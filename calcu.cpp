df122c1df4278fd0fec3edcd377bd78f4c940bd1
Jhaslyn Gerochi <escariosjhas02@gmail.com>
2021-09-29 10:28:04 +0800

    Added Multiplication and Division Feature. Created a New File.

--- calcu.cpp
+++ calcu.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    int firstNumber, secondNumber, sumOfTwoNumbers, diffOfTwoNmbers;
+    int firstNumber, secondNumber, sumOfTwoNumbers, diffOfTwoNmbers, prodOfTwoNumbers, quotientOfTwoNumbers;
     
     cout << "Enter first number: ";
     cin >> firstNumber;
@@ -13,9 +13,13 @@ int main()
 
     // sum of two numbers is stored in variable sumOfTwoNumbers
     sumOfTwoNumbers = firstNumber + secondNumber;
-    diffOfTwoNmbers = firstNumber + secondNumber;
+    diffOfTwoNmbers = firstNumber - secondNumber;
+    prodOfTwoNumbers = firstNumber * secondNumber;
+    quotientOfTwoNumbers = firstNumber / secondNumber;
     // Prints sum 
     cout << endl << firstNumber << " + " <<  secondNumber << " = " << sumOfTwoNumbers;     
-    cout << endl << firstNumber << " - " <<  secondNumber << " = " << diffOfTwoNmbers;    
+    cout << endl << firstNumber << " - " <<  secondNumber << " = " << diffOfTwoNmbers;
+	cout << endl << firstNumber << " x " <<  secondNumber << " = " << prodOfTwoNumbers;  
+	cout << endl << firstNumber << " x " <<  secondNumber << " = " << quotientOfTwoNumbers;        
     return 0;
 }
